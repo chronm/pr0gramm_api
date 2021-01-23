79d36f2cde5a87ee8ba5830b135676f94ce61c7f
Chronm <info@chronm.dev>
2021-01-24 00:50:45 +0100

    Fix ItemComment

--- example/pr0gramm_api_example.dart
+++ example/pr0gramm_api_example.dart
@@ -1,11 +1,12 @@
-import 'package:cookie_jar/cookie_jar.dart';
-import 'package:dio/dio.dart';
+import 'dart:convert';
+
 import 'package:pr0gramm_api/pr0gramm_api.dart';
-import 'package:pr0gramm_api/src/util/pr0gramm_cookie_manager.dart';
 
 void main() async {
-  var cookieManager = Pr0grammCookieManager(DefaultCookieJar());
-  var dio = Dio()..interceptors.add(cookieManager);
-  var pr0gramm = Pr0gramm(dio);
-  await pr0gramm.getCaptcha();
+  // var cookieManager = Pr0grammCookieManager(DefaultCookieJar());
+  // var dio = Dio()..interceptors.add(cookieManager);
+  // var pr0gramm = Pr0gramm(dio);
+  // await pr0gramm.getCaptcha();
+
+  Sync.fromJson(jsonDecode('{"log": "3K0GAAzcrQYADg=="}'));
 }
--- lib/src/api/items/model/info/item_comment.dart
+++ lib/src/api/items/model/info/item_comment.dart
@@ -1,10 +1,12 @@
 import 'package:json_annotation/json_annotation.dart';
+import 'package:pr0gramm_api/src/util/converters.dart';
 
 import '../../../../../pr0gramm_api.dart';
 
 part 'item_comment.g.dart';
 
 @JsonSerializable(explicitToJson: true)
+@DateTimeConverter()
 class ItemComment {
   int id;
   int parent;
